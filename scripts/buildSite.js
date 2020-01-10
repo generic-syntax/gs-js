02720b9a2822f40b9b11f8bed9bff6ab7798117d
dar <david.rivron@kelis.fr>
2020-01-10 09:23:44 +0100

    review buildSite.js

--- scripts/buildSite.js
+++ scripts/buildSite.js
@@ -7,13 +7,14 @@ const terser = require('terser');
 (async () => {
 	try {
 		const baseDir = `${__dirname}/..`;
+		const siteDir = `${baseDir}/site`;
 		const distDir = `${baseDir}/dist`;
 		await fs.remove(distDir);
 
 		const files = await glob('**', { ignore: [ 'mod/**', '**/*.map', '**/*.ts', '**/*.tsx' ], nodir: true, cwd: `${baseDir}/site` });
-		await Promise.all(files.map((file) => fs.copy(`${baseDir}/site/${file}`, `${distDir}/${file}`)));
+		await Promise.all(files.map((file) => fs.copy(`${siteDir}/${file}`, `${distDir}/${file}`)));
 
-		const bundle = await rollup.rollup({input: 'site/mod/index.js'});
+		const bundle = await rollup.rollup({input: `${siteDir}/mod/index.js`});
 		const {output: [{code: bundledCode}]} = await bundle.generate({file: `${distDir}/mod/index.js`});
 		const {code: minifiedCode} = terser.minify(bundledCode);
 		await fs.outputFile(`${distDir}/mod/index.js`, minifiedCode);
